c7258fd497170243f95c4c2f93824f33508cc356
bandyandy <andyb23890@gmail.com>
2023-04-11 00:26:19 +0000

    Chapter 10 in the Lab

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+  Student Name: Andy Bowles
+  Date: 04/10/2023
+  File Name: script.js
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
